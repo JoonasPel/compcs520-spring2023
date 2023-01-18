6a4c002d02769eabb352f87e44e977f4f77744f4
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-01-18 04:35:10 +0200

    Exercise 2.2

--- elm/players_CRUD/src/Main.elm
+++ elm/players_CRUD/src/Main.elm
@@ -17,12 +17,10 @@ type alias Player =
     , isActive : Bool
     }
 
-
 type alias Model =
     { players : List Player
     , newPlayer : Player
     }
-
 
 type Msg
     = SetName String
@@ -42,21 +40,57 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         SetName name ->
-            model
+            let 
+                oldPlayer = model.newPlayer
+                newPlayer = { oldPlayer | name = name}
+            in
+                { model | newPlayer = newPlayer }
 
         AddPlayer ->
-            model
+            let 
+                addedPlayer = model.newPlayer
+                initialPlayer = initPlayer (addedPlayer.id + 1)
+            in
+            { 
+                model | players = model.players ++ [addedPlayer] , 
+                newPlayer = initialPlayer
+            }
 
         DeletePlayer id ->
-            model
+            let
+                existingPlayers = List.filter (\player -> player.id /= id) model.players
+            in
+                { model | players = existingPlayers}
 
         ModifyPlayer id status ->
-            model
+            let
+                newPlayers = List.map (\p -> if p.id == id then { p | isActive = status} else p) model.players             
+            in
+                { model | players = newPlayers}
 
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Elm Exercise: Players CRUD" ]
+    div [] 
+        [ h1 [] [ text "Elm Exercise: Players CRUD" ] ,
+
+          Html.form [id "submit-player", onSubmit AddPlayer]
+          [ 
+          input [id "input-player", type_ "text", value model.newPlayer.name, placeholder "Enter new player", onInput SetName] [] ,
+          button [id "btn-add" , type_ "submit"] [ text "Add" ]
+          ] ,
+
+          ol [id "players-list"]
+            (List.map (\player -> li [id ("player-" ++ String.fromInt player.id)]
+                        [
+                        div [class "player-name"] [text player.name] ,
+                            label [class "player-status"] [input [class "player-status", type_ "checkbox", checked player.isActive, 
+                            onCheck (ModifyPlayer player.id)] [text "active"], span [class "checkmark"] [] ]
+                        , button [class "btn-delete", onClick (DeletePlayer player.id)] [text "Delete"]
+                        ]) model.players)       
+        ]
+    
+    
 
 
 main : Program () Model Msg
