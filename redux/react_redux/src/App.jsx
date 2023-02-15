f377ee7b729daf4526b900f48214c0e790a7d2c4
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-02-15 04:21:37 +0200

    Exercise 5.3

--- redux/react_redux/src/App.jsx
+++ redux/react_redux/src/App.jsx
@@ -1,27 +1,74 @@
 /** @format 
  * 
  * Student instructions:
- * Copy paste the App.jsx file from the previous exercises into this file. In this exercise, you will be introducing redux, which is a state management library that allows you to manage the state of your application in a single store. The store is a single source of truth for the state of your application, and it is the only place where the state can be updated. 
+ * Copy paste the App.jsx file from the previous exercises into this file. In this exercise, you will be introducing redux, which is a state 
+ * management library that allows you to manage the state of your application in a single store. The store is a single source of truth for the state 
+ * of your application, and it is the only place where the state can be updated. 
  * 
- * The fetch functions will start using action creators from now on. Each action creator will be responsible for updating the redux store with the data from the request. You can find the template files for the action creators in the src/redux/actionCreators folder. It is your job to implement them, as well as the reducers that will be used to update the store. The reducers can be found in the src/redux/reducers folder.
+ * The fetch functions will start using action creators from now on. Each action creator will be responsible for updating the redux store with the data 
+ * from the request. You can find the template files for the action creators in the src/redux/actionCreators folder. It is your job to implement them, 
+ * as well as the reducers that will be used to update the store. The reducers can be found in the src/redux/reducers folder.
  * 
-  Hint: Use the provided REQ_STATUS object to update the request status when necessary. "loading" for when the request is in progress, "success" for when the request is successful, and "error" for when the request has failed. The REQ_STATUS object is imported from the src/redux/constants.js file.
+  Hint: Use the provided REQ_STATUS object to update the request status when necessary. "loading" for when the request is in progress, "success" for 
+  when the request is successful, and "error" for when the request has failed. The REQ_STATUS object is imported from the src/redux/constants.js file.
 
 */
 
+// --------- IMPORTS ---------
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { REQ_STATUS } from './redux/constants.js';
+//import { setStatus, setPlayers, setSelectedPlayer, clearSelectedPlayer } from './redux/actionCreators'
+import { setStatus } from './redux/actionCreators/statusActions';
+import { setPlayers } from './redux/actionCreators/playersActions';
+import { setSelectedPlayer, clearSelectedPlayer } from './redux/actionCreators/selectedPlayerActions';
+
+// --------- COMPONENT IMPORTS ---------
 import { ListPlayers } from './components/ListPlayers.jsx';
 import { SelectedPlayer } from './components/SelectedPlayer.jsx';
 import { RequestStatus } from './components/RequestStatus.jsx';
 
+const URL = "http://localhost:3001/api/players/";
 
-function App () {
-  return (
-    <>
-      <RequestStatus />
-      <ListPlayers />
-      <SelectedPlayer />
-    </>
-  );
+function App() {
+  const dispatch = useDispatch();
+
+	// --------- FUNCTIONS ---------
+	useEffect(() => { fetchPlayers() }, []);
+
+	function fetchPlayers() {
+		dispatch(setStatus(REQ_STATUS.loading));
+		fetch(URL)
+		.then(response => response.json())
+		.then(data => dispatch(setPlayers(data)))
+		.then(() => { dispatch(setStatus(REQ_STATUS.success)); })
+
+		.catch((error) => {
+			console.log(error);
+      dispatch(setStatus(REQ_STATUS.error));
+		});
+	};
+
+	function fetchPlayer(playerId) {
+		dispatch(setStatus(REQ_STATUS.loading));
+		fetch(URL + playerId)
+		.then(response => response.json())
+		.then(data => dispatch(setSelectedPlayer(data)))
+		.then(() => { dispatch(setStatus(REQ_STATUS.success)); })
+
+		.catch((error) => {
+			console.log(error);
+			dispatch(setStatus(REQ_STATUS.error));
+		});
+	};
+
+	return (
+		<>
+			<RequestStatus />
+			<ListPlayers selectPlayer={fetchPlayer}/>
+			<SelectedPlayer />
+		</>
+	);
 }
 
 export default App;
--- redux/react_redux/src/components/ListPlayers.jsx
+++ redux/react_redux/src/components/ListPlayers.jsx
@@ -6,11 +6,28 @@
  * BEWARE: Only the selectPlayer function is passed as a prop from now on. The players data is fetched from the redux store.
  *
  */
-export const ListPlayers = ({ players, selectPlayer }) => {
-  return (
-    <div>
-      <h2>List of players</h2>
-      TODO: ListPlayers
-    </div>
+import { useSelector } from "react-redux";
+import { ListPlayer } from './ListPlayer';
+
+export const ListPlayers = ({ selectPlayer }) => {
+  const players = useSelector((state) => state.players)
+
+  if (!players) return (
+  <div>
+    <h2>List of players</h2>
+    <ul id="players-list">
+    </ul>
+  </div>
   );
+
+	return (
+		<div>
+			<h2>List of players</h2>
+			<ul id="players-list">
+				{players.map(player => {
+					return <ListPlayer key={player.id} name={player.name} id={player.id} onClick={selectPlayer}/>
+				})}
+			</ul>
+		</div>
+	);
 };
