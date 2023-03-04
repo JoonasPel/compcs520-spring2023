aa5fff2f107c36f11aa7640d71c1c3640d6de1bc
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-03-04 13:11:53 +0200

    auth actions implemented

--- assignment/frontend/src/redux/actionCreators/authActions.js
+++ assignment/frontend/src/redux/actionCreators/authActions.js
@@ -1,5 +1,6 @@
 /** @format */
 
+import axios from 'axios';
 import {
 	CLEAR_ORDERS,
 	CLEAR_USERS,
@@ -14,20 +15,36 @@ import { createNotification } from './notificationsActions';
 //You can use this  regex for email validation, taken from here: https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
 const validEmailRegex =
 	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const BASEURL = 'http://localhost:3001/api/';
+
 
 //AUTH (THUNK) ACTION CREATORS
 /**
  *
- * @description Asynchronous thunk that uses backends /api/check-status path to check whether or not there is the correct browser-cookie and whether or not that browser-cookie is valid. If it's succesful, Dispatches
+ * @description Asynchronous thunk that uses backends /api/check-status path to check whether or not there is
+ * the correct browser-cookie and whether or not that browser-cookie is valid. If it's succesful, Dispatches
  * 1) INIT_AUTH with user as payload.
- * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
+ * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state
+ * along with the error message from db as an unsuccessfull message.
  *
  * @returns {Function}
  */
 
-export const initAuth = () => {};
+export const initAuth = () => {
+	return async (dispatch) => {
+		const url = BASEURL + 'check-status';
+		const response = await axios.get(url);
+		if (response.status == 200) {
+			dispatch({ type: INIT_AUTH, payload: response.data.user});
+		} else {
+			dispatch(createNotification({ message: response.statusText, isSuccess: false }));
+		}
+	};
+};
+
 /**
- * @description When used, creates an action that awaits for a successful login-response from server, before dispatches the received credentials as payload to the reducers.
+ * @description When used, creates an action that awaits for a successful login-response from server,
+ * before dispatches the received credentials as payload to the reducers.
  * Light validation of the loginCreds is done before sending the request to the server.
  * The light validation checks:
  * 1. Invalid email: dispatches NEW_NOTIFICATION with error message invalidAuthMsg.email
@@ -36,12 +53,48 @@ export const initAuth = () => {};
  * If validation succeeds, dispatches
  * 1) INIT_AUTH with user as payload
  * 2) succesfull notification with authMsg.welcomeBack as message.
- * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull notification.
+ * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends
+ * notification state along with the error message from db as an unsuccessfull notification.
  *
  * @param {Object} logInCreds - The credentials used to login, contains username and password
  * @returns {Function} action
  */
-export const logIn = (logInCreds) => {};
+export const logIn = (logInCreds) => {
+	return async (dispatch) => {
+		const email = logInCreds.email;
+		const password = logInCreds.password;		
+		 if (password.length < 10) {
+			dispatch(createNotification({ 
+				message: invalidAuthMsg.password, 
+				isSuccess: false 
+			}));
+		 } else if (!String(email).toLowerCase().match(validEmailRegex)) {
+			dispatch(createNotification({
+				message: invalidAuthMsg.email,
+				isSuccess: false
+			}));
+		 } else {
+			const url = BASEURL + 'login';
+			const options = { data: { email, password }};
+			const response = await axios.post(url, options);
+			if (response.status == 200) {
+				dispatch({
+					type: INIT_AUTH,
+					payload: response.data.user
+				});
+				dispatch(createNotification({
+					message: authMsg.welcomeBack,
+					isSuccess: true
+				}));
+			} else {
+				dispatch(createNotification({
+					message: response.statusText,
+					isSuccess: false
+				}));	
+			}
+		 }	
+	};
+};
 
 /**
  * @description Asynchronous thunk that awaits for a successful logout-response from server, before dispatches
@@ -52,22 +105,85 @@ export const logIn = (logInCreds) => {};
  * 4) NEW_NOTIFICATION with succesfull message from the backend as payload to the reducers.
  * @returns {Function}
  */
-export const logOut = () => {};
+export const logOut = () => {
+	return async (dispatch) => {
+		const url = BASEURL + 'logout';
+		const response = await axios.get(url);
+		if (response.status == 200) {
+			dispatch({ type: REMOVE_AUTH });
+			dispatch({ type: CLEAR_ORDERS });
+			dispatch({ type: CLEAR_USERS });
+			dispatch(createNotification({ message: "Logged out.", isSuccess: true }));
+		}
+	};
+};
 
 /**
- * @description Asynchronous thunk that handles registeration events. Handles validation for registerCreds (check Login and Registration validation from assignment instructions).
+ * @description Asynchronous thunk that handles registeration events. Handles validation for
+ * registerCreds (check Login and Registration validation from assignment instructions).
  * Validation checks:
  * 1. Invalid email: dispatches NEW_NOTIFICATION with error message invalidAuthMsg.email
  * 2. Password < 10: dispatches NEW_NOTIFICATION with error message invalidAuthMsg.password
  * 3. name < 3: dispatches NEW_NOTIFICATION with error message invalidAuthMsg.name
- * 4. password missmatch: dispatches NEW_NOTIFICATION with error message invalidAuthMsg.passwordConfirmation
+ * 4. password missmatch: dispatches NEW_NOTIFICATION with error message
+ * invalidAuthMsg.passwordConfirmation
  *
- * If the validation succeeds, the registerCreds are sent to the server. If the response is ok, Dispatches
+ * If the validation succeeds, the registerCreds are sent to the server. If the response
+ * is ok, Dispatches
  * 1) an INIT_AUTH-type action to reducers with the received user as payload.
  * 2) a successful NEW_NOTIFICATION-type action to reducers with validAuth.welcome(name) as message.
  *
- *  If the server responds with an error, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull notification. If the error itself is an object, then it should pass whatever is inside the object.
+ *  If the server responds with an error, it only dispatches a NEW_NOTIFICATION-type action
+ * to the frontends notification state along with the error message from db as an unsuccessfull
+ * notification. If the error itself is an object, then it should pass whatever is inside the object.
  * @param registerCreds - The data of the user
  * @returns {Function}
  */
-export const register = (registerCreds) => {};
+export const register = (registerCreds) => {
+	return async (dispatch) => {
+		const email = registerCreds.email;
+		const name = registerCreds.name;
+		const password = registerCreds.password;	
+		const passwordConfirmation = registerCreds.passwordConfirmation;
+		 if (!String(email).toLowerCase().match(validEmailRegex)) {
+			dispatch(createNotification({ 
+				message: invalidAuthMsg.email, 
+				isSuccess: false 
+			}));
+		 } else if (password.length < 10) {
+			dispatch(createNotification({
+				message: invalidAuthMsg.password,
+				isSuccess: false
+			}));
+		 } else if (name.length < 3) {
+			dispatch(createNotification({
+				message: invalidAuthMsg.name,
+				isSuccess: false
+			}));
+		 } else if (password !== passwordConfirmation) {
+			dispatch(createNotification({
+				message: invalidAuthMsg.passwordConfirmation,
+				isSuccess: false
+			}));
+		 } else {
+			const url = BASEURL + 'register';
+			const options = { data: { name, email, password }};
+			const response = await axios.post(url, options);
+			if (response.status == 201) {
+				dispatch({
+					type: INIT_AUTH,
+					payload: response.data.user
+				});
+				dispatch(createNotification({
+					message: authMsg.welcome(response.data.user.name),
+					isSuccess: true
+				}));
+			} else {
+				dispatch(createNotification({
+					message: response.statusText,
+					isSuccess: false
+				}));	
+			}
+		 }	
+	};
+};
